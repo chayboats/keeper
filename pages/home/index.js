1cfe0eb858f3713b3eb8293e58126ee7f9afe9a9
Chaley Boatmon <chaleyboatmon@gmail.com>
2023-06-30 21:04:12 -0400

    Add header to home

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,31 +1,19 @@
-import Link from "next/link";
-import Header from "./internals/components/Header";
-import SecondHeader from "./internals/components/SecondHeader";
-import styles from "./internals/styles/home.css";
+import Header from './internals/components/Header';
+import './internals/styles/home.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [hideDropdown, setHideDropdown] = useState(true);
+
+  function toggleDropdownClass() {
+    setHideDropdown(!hideDropdown);
+  }
   return (
-    <div
-      style={{
-        textAlign: "center",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <h1 style={{ fontSize: "10rem" }}>Welcome</h1>
-      <Link style={{ fontSize: "2rem" }} href="/">
-        Logout
-      </Link>
+    <div class="home">
+      <Header
+        onClick={toggleDropdownClass}
+        dropdownClass={hideDropdown ? 'hide' : 'show'}
+      />
     </div>
   );
-}
-
-{
-  /* <div class="home">
-    
-    <Header />
-    <SecondHeader />
-  </div> */
 }
--- pages/home/internals/components/Header.js
+++ pages/home/internals/components/Header.js
@@ -1,70 +1,34 @@
-// export default function Header() {
-//   return (
-//     <header className="p-3 mb-3 border-bottom">
-//       <div className="container">
-//         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-//           <a
-//             href="/"
-//             className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
-//           >
-//             <svg
-//               className="bi me-2"
-//               width="40"
-//               height="32"
-//               role="img"
-//               aria-label="Bootstrap"
-//             ></svg>
-//           </a>
+import Link from 'next/link';
+export default function Header(props) {
+  const {onClick, dropdownClass} = props;
 
-//           <div className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-//             <span className="nav-link px-2 link-secondary">Keeper</span>
-//           </div>
-
-//           <div className="dropdown text-end">
-//             <a
-//               href="#"
-//               className="d-block link-dark text-decoration-none dropdown-toggle show"
-//               id="dropdownUser1"
-//               data-bs-toggle="dropdown"
-//               aria-expanded="true"
-//             >
-//               <img
-//                 src="https://github.com/mdo.png"
-//                 alt="mdo"
-//                 width="32"
-//                 height="32"
-//                 className="rounded-circle"
-//               />
-//             </a>
-//             <ul
-//               className="dropdown-menu text-small show"
-//               aria-labelledby="dropdownUser1"
-//               style={{
-//                 position: "absolute",
-//                 inset: "0px auto auto 0px",
-//                 margin: "0px",
-//                 transform: "translate(0px, 34px)",
-//               }}
-//               data-popper-placement="bottom-start"
-//             >
-//               <li>
-//                 <a className="dropdown-item" href="#">
-//                   New project...
-//                 </a>
-//               </li>
-             
-//               <li>
-//                 <hr className="dropdown-divider" />
-//               </li>
-//               <li>
-//                 <a className="dropdown-item" href="#">
-//                   Sign out
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
+  return (
+    <header className="header">
+      <h1>Keeper</h1>
+      <div className="profile">
+        <img
+          src="https://github.com/mdo.png"
+          alt="mdo"
+          width="32"
+          height="32"
+          className="rounded-circle"
+          onClick={onClick}
+        />
+        <ul className={dropdownClass}>
+          <li id="create-note">+ Create Note</li>
+          <li id="line">
+            <hr />
+          </li>
+          <li id="logout">
+            <Link
+              className="logout"
+              href="/"
+            >
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
